82eceb604d627c890cf525672ccfc3e3722ef37e
ilyasZK <ilyaszahaf2002@gmail.com>
2023-12-18 14:58:04 +0100

    <feat> mise en place de tailwind + CRUD atelier

---
+++ src/Form/AtelierType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Atelier;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AtelierType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('heureDebut')
+            ->add('heureFin')
+            ->add('intervenants')
+            ->add('salle')
+            ->add('inscription')
+            ->add('secteur')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Atelier::class,
+        ]);
+    }
+}
