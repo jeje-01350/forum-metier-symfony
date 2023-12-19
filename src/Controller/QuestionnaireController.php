398c68002eb63251af229cfe0d76a971875ea872
RuddyM <ruddy.morel@outlook.com>
2023-12-19 12:52:38 +0100

    <feat> change questionnaire

--- src/Controller/QuestionnaireController.php
+++ src/Controller/QuestionnaireController.php
@@ -1,25 +1,60 @@
 <?php
 
+// src/Controller/QuestionnaireController.php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class QuestionnaireController extends AbstractController
 {
     #[Route('/questionnaire', name: 'app_questionnaire')]
     public function fillQuestionnaire(Request $request): Response
     {
-        $form = $this->createFormBuilder()
-            ->add('question1', null, ['label' => 'Question 1'])
-            ->add('question2', null, ['label' => 'Question 2'])
-            ->getForm();
+        // Questions statiques
+        $questions = [
+            ['text' => 'Quel est votre avis sur la qualité des ateliers ?', 'type' => 'ouvert'],
+            ['text' => 'Avez-vous trouvé les informations utiles ?', 'type' => 'ferme'],
+            ['text' => 'Quels sont les points forts de l\'événement ?', 'type' => 'ouvert'],
+            ['text' => 'Recommanderiez-vous cet événement à vos amis ?', 'type' => 'ferme'],
+            ['text' => 'Quelles suggestions avez-vous pour améliorer le forum ?', 'type' => 'ouvert'],
+        ];
+
+        // Créer le formulaire en ajoutant dynamiquement les champs pour chaque question
+        $formBuilder = $this->createFormBuilder();
+
+        foreach ($questions as $index => $question) {
+            $fieldName = 'question_' . $index;
+
+            if ($question['type'] === 'ouvert') {
+                $formBuilder->add($fieldName, TextType::class, [
+                    'label' => $question['text'],
+                    'required' => true,
+                ]);
+            } elseif ($question['type'] === 'ferme') {
+                $formBuilder->add($fieldName, ChoiceType::class, [
+                    'label' => $question['text'],
+                    'choices' => ['Oui' => true, 'Non' => false],
+                    'expanded' => true,
+                    'multiple' => false,
+                    'required' => true,
+                ]);
+            }
+        }
+
+        $form = $formBuilder->getForm();
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Logique de traitement du formulaire
+            // ...
+
             return $this->redirectToRoute('app_home'); // Remplacez 'app_home' par la route souhaitée
         }
 
