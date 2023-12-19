694e609d284199ea2dced9786640c18eaa5542cf
ilyasZK <ilyaszahaf2002@gmail.com>
2023-12-19 12:49:53 +0100

    <feat> pb modifcation atelier

--- src/Form/SecteurType.php
+++ src/Form/SecteurType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Forum;
 use App\Entity\Secteur;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,8 +15,12 @@ class SecteurType extends AbstractType
     {
         $builder
             ->add('nom')
-            ->add('descritpion')
-            ->add('forum')
+            ->add('description')
+            ->add('forum', EntityType::class, [
+                'class' => Forum::class,
+                'choice_label' => 'nom', // Remplacez 'nom' par la propriété à afficher dans la liste déroulante
+                'placeholder' => 'Sélectionnez un forum', // Optionnel : texte par défaut
+            ])
         ;
     }
 
